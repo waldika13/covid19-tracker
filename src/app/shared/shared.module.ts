ff1cf95ae5a26066a4c03dd1fe0217722e873653
Waldika Irawan <waldika.irawan00@gmail.com>
2023-08-07 20:25:29 +0700

    Ubah NavBar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { RouterModule } from '@angular/router';
 
@@ -8,7 +7,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    SidebarComponent,
     TopbarComponent
   ],
   imports: [
@@ -16,7 +14,6 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   exports: [
-    SidebarComponent,
     TopbarComponent
   ]
 })
